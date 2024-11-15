015b1859b654ff9af119ff38d653a2bfdfbc3c3f
Setanjan Roy <setanjan123@gmail.com>
2024-11-15 19:42:10 +0530

    fixes

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 import { pipeline } from 'https://cdn.jsdelivr.net/npm/@huggingface/transformers@3.0.2';
 
 
-chrome.runtime.onInstalled.addListener(() => {
+chrome.runtime.onInstalled.addListener(async() => {
   // Create a parent menu item
   chrome.contextMenus.create({
     id: "summariseMenu",
@@ -11,20 +11,13 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 chrome.contextMenus.onClicked.addListener(async(info, tab) => {
-  const pipe = await pipeline('summarization','Xenova/distilbart-cnn-6-6');
-  switch (info.menuItemId) {
-    case "summariseMenu":
-      // Delegate to content script for page interaction
-      try {
-        chrome.storage.local.set({ selectedText: 'Generating Summary Please Wait...' }, async () => {
-          chrome.action.openPopup(); // Opens the popup window
-          const out = await pipe(info.selectionText);
-          const summary = out[0].summary_text;
-          chrome.storage.local.set({ selectedText: summary }, () => {});
-        });
-      } catch (error) {
-        console.error('Failed to send message to content script:', error);
-      }
-      break;
+  chrome.action.openPopup(); // Opens the popup window
+  const pipe = await pipeline('summarization','Xenova/distilbart-cnn-12-6');
+  const out = await pipe(info.selectionText);
+  const summary = out[0].summary_text;
+  chrome.storage.local.set({ selectedText: summary }, () => {});
+  try {
+  } catch (error) {
+    console.error('Failed to send message to content script:', error);
   }
 });
